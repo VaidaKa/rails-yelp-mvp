bafd8530e43ec6588fb3f6c69808021345effb87
Vaida Kareivaite <vkkareivaite@gmail.com>
2024-05-20 23:19:16 +0100

    yelp restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+restaurants = [
+  { name: "The Ledbury", address: "127 Ledbury Rd, Notting Hill, London W11 2AQ, UK", phone_number: "+44 20 7792 9090", category: "french" },
+  { name: "Dishoom", address: "12 Upper St Martin's Ln, London WC2H 9FB, UK", phone_number: "+44 20 7420 9320", category: "japanese" },
+  { name: "Pizza Pilgrims", address: "11 Dean St, Soho, London W1D 3RP, UK", phone_number: "+44 20 7287 8964", category: "italian" },
+  { name: "A. Wong", address: "70 Wilton Rd, Pimlico, London SW1V 1DE, UK", phone_number: "+44 20 7828 8931", category: "chinese" },
+  { name: "Belgo Centraal", address: "50 Earlham St, London WC2H 9LJ, UK", phone_number: "+44 20 7813 2233", category: "belgian" }
+]
+
+restaurants.each do |attributes|
+  Restaurant.create!(attributes)
+end
+
+puts "Seeded #{Restaurant.count} restaurants."
